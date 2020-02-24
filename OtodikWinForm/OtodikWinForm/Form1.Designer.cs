ee72cbb0cd4182fb8362df5297f4c6b6e111e918
kinga97 <kinga.kucsera97@gmail.com>
2020-02-24 12:55:04 +0100

    négyzetgyök, négyzetszám

---
+++ OtodikWinForm/OtodikWinForm/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace OtodikWinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Hozzaad = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 148);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 66);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Hozzaad
+            // 
+            this.Hozzaad.Location = new System.Drawing.Point(158, 80);
+            this.Hozzaad.Name = "Hozzaad";
+            this.Hozzaad.Size = new System.Drawing.Size(75, 23);
+            this.Hozzaad.TabIndex = 5;
+            this.Hozzaad.Text = "Hozzáad";
+            this.Hozzaad.UseVisualStyleBackColor = true;
+            this.Hozzaad.Click += new System.EventHandler(this.Hozzaad_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(251, 171);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 100);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(140, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(252, 56);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 95);
+            this.listBox2.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(405, 285);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.Hozzaad);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Hozzaad;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ OtodikWinForm/OtodikWinForm/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OtodikWinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Hozzaad_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            comboBox1.Items.Clear();
+            /*
+            //Írja ki a szám osztóit a listában
+            int bemeno = Int32.Parse(textBox1.Text);
+            string masikbemeno = textBox2.Text;
+            for (int i = 1; i <= bemeno; i++)
+            {
+                if (bemeno % i == 0)
+                {
+                    listBox1.Items.Add(i);
+                    //Írja ki a combobox-ban, a számokat ami tartalmazza a masikbemeno értékét
+                    if (i.ToString().Contains(masikbemeno))
+                    {
+                        comboBox1.Items.Add(i);
+                    }
+                    
+                } 
+            }
+            */
+
+            //Ha a négyzetgyöknél nagyobb az osztó akkor comboboxba menjen, ha kisebb vagy egyenlő akkor a listbox2-be
+            int bemeno = Int32.Parse(textBox1.Text);
+            int negyzetgyok = (int)Math.Floor(Math.Sqrt(bemeno));
+            for (int i = 1; i <= bemeno; i++)
+            {
+                if (bemeno % i == 0)
+                {
+                    listBox1.Items.Add(i);
+                    if (i <= negyzetgyok)
+                    {
+                        listBox2.Items.Add(i);
+                    }
+                    else if (i > negyzetgyok)
+                    {
+                        comboBox1.Items.Add(i);
+                    }
+                    
+                }
+            }
+
+            if (comboBox1.Items.Count == listBox2.Items.Count)
+            {
+                textBox2.Text = "A szám nem négyzetszám.";
+            }
+            else
+            {
+                textBox2.Text = "A szám négyzetszám.";
+            }
+        }
+    }
+}
