67f755b5d51d1cadd2e5aea0a6d5a3b509ac0cf5
kinga97 <kinga.kucsera97@gmail.com>
2020-02-24 12:00:19 +0100

    listbox, combobox

---
+++ NegyedikWinForm/NegyedikWinForm/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace NegyedikWinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.ValasztoGomb = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.HozzaadGomb = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "Első",
+            "Második",
+            "Harmadik",
+            "Negyedik",
+            "Ötödik",
+            "Hatodik"});
+            this.listBox1.Location = new System.Drawing.Point(21, 164);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 0;
+            // 
+            // ValasztoGomb
+            // 
+            this.ValasztoGomb.Location = new System.Drawing.Point(172, 187);
+            this.ValasztoGomb.Name = "ValasztoGomb";
+            this.ValasztoGomb.Size = new System.Drawing.Size(94, 32);
+            this.ValasztoGomb.TabIndex = 1;
+            this.ValasztoGomb.Text = "Választó gomb";
+            this.ValasztoGomb.UseVisualStyleBackColor = true;
+            this.ValasztoGomb.Click += new System.EventHandler(this.ValasztoGomb_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(274, 32);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(104, 107);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(21, 84);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // HozzaadGomb
+            // 
+            this.HozzaadGomb.Location = new System.Drawing.Point(181, 81);
+            this.HozzaadGomb.Name = "HozzaadGomb";
+            this.HozzaadGomb.Size = new System.Drawing.Size(75, 23);
+            this.HozzaadGomb.TabIndex = 4;
+            this.HozzaadGomb.Text = "Hozzáad";
+            this.HozzaadGomb.UseVisualStyleBackColor = true;
+            this.HozzaadGomb.Click += new System.EventHandler(this.HozzaadGomb_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(393, 32);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(101, 108);
+            this.listBox2.TabIndex = 5;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(284, 187);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 384);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.HozzaadGomb);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.ValasztoGomb);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button ValasztoGomb;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button HozzaadGomb;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ NegyedikWinForm/NegyedikWinForm/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NegyedikWinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ValasztoGomb_Click(object sender, EventArgs e)
+        {
+            //textBox1.Text = listBox1.SelectedItem.ToString(); //Egy elem kiválasztása esetén
+            string kimeno = "";
+            foreach (var item in listBox1.SelectedItems)
+            {
+                //kimeno += item.ToString() + "\r\n"; //Listbox1-be írja ki
+                //listBox1.Items.Add(item); //Csak a listbox2-be jelenítse meg
+                comboBox1.Items.Add(item);
+            }
+            textBox1.Text = kimeno;
+            
+        }
+
+        private void HozzaadGomb_Click(object sender, EventArgs e)
+        {
+            string hozzaad = textBox2.Text;
+            listBox1.Items.Add(hozzaad);
+        }
+    }
+}
